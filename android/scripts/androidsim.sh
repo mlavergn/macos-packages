e14b360024386e11b05085fa5def7ab6941082c5
Marc Lavergne <mlavergn@gmail.com>
2020-04-19 22:33:24 -0400

    :hammer: android additional sdk tweaks

---
+++ android/scripts/androidsim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ ! -d "$HOME/.android" ]; then
+	avdmanager create avd --force -d pixel_xl -n PixelXL --abi google_apis_playstore/x86_64 -k 'system-images;android-{VERSION};google_apis_playstore;x86_64'
+fi
+
+emulator -sysdir /usr/local/android/system-images/android-{VERSION}/google_apis_playstore/x86_64 -avd PixelXL -skin 1440x2560
